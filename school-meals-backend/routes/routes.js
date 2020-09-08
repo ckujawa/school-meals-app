f0c3e230040783d0b5419fa3caf0a19517933be4
Chris Kujawa <ckujawa@nhkujawas.com>
2020-09-08 07:35:57 -0400

    Chnages to model and schema

--- school-meals-backend/routes/routes.js
+++ school-meals-backend/routes/routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const appDataController = require('../controllers/appDataController');
 
 router.post('/submit', appDataController.createAppData);
+router.get('/getNewApps', appDataController.getNewAppData);
 
 module.exports = router;
