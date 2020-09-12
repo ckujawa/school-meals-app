895a9a431bf93cf47bb5f59c0c42fd0be2e6e24a
Chris Kujawa <ckujawa@nhkujawas.com>
2020-09-12 07:15:33 -0400

    start building admin ui

--- school-meals-backend/controllers/appDataController.js
+++ school-meals-backend/controllers/appDataController.js
@@ -3,7 +3,6 @@ const AppData = mongoose.model('AppData');
 
 exports.createAppData = (req, res) => {
     let reqData = req.body;
-    console.log(reqData);
     let appData = new AppData({
         field1: reqData,
         status: 'new'
@@ -18,8 +17,8 @@ exports.createAppData = (req, res) => {
     })
 }
 
-exports.getNewAppData = (req, res) => {
-    const newApps = AppData.find({ status: 'new' }, (err, users) =>
+exports.getNewAppData = async (req, res) => {
+    const newApps = await AppData.find({ status: 'new' }, (err, users) =>
     {
         if (err) {
             res.status(500).send("An error has occurred in processing your request. We are working to resolve it as quickly as possible.")
@@ -30,4 +29,18 @@ exports.getNewAppData = (req, res) => {
             res.status(200).send(userMap)
         }
     })
+}
+
+exports.getNewApps = async (req, res) => {
+    console.log('entering getNewApps...')
+    const apps = await AppData.find({ status: 'new' });
+    console.log(`found ${apps.length} apps...`)
+    const numOfApps = apps.length - 1;
+
+    // if (!apps.length) {
+    //     req.flash('info', `You asked for more applications than we currently have, so I'm sending you to the last one...`);
+    //     res.redirect(`/admin/apps/page/${numOfApps}`);
+    //     return;
+    // }
+    res.render('apps', {title: 'Applications for Processing', apps})
 }
--- school-meals-backend/routes/routes.js
+++ school-meals-backend/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const appDataController = require('../controllers/appDataController');
+const adminController = require('../controllers/adminController')
 
 router.post('/submit', appDataController.createAppData);
-router.get('/getNewApps', appDataController.getNewAppData);
+router.get('/getNewApps', appDataController.getNewApps);
+
+router.get('/admin', adminController.loginForm)
 
 module.exports = router;
